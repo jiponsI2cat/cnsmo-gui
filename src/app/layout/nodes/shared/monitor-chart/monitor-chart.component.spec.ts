7b705b45f5d74a96c6e673e2f5dea1305cab2f19
wfriscina <william.friscina@i2cat.net>
2018-02-15 12:11:03 +0100

    Create component monitoring incoming

---
+++ src/app/layout/nodes/shared/monitor-chart/monitor-chart.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MonitorChartComponent } from './monitor-chart.component';
+
+describe('MonitorChartComponent', () => {
+  let component: MonitorChartComponent;
+  let fixture: ComponentFixture<MonitorChartComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ MonitorChartComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MonitorChartComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should be created', () => {
+    expect(component).toBeTruthy();
+  });
+});
